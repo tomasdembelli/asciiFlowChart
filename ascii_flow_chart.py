9a76d0b0de3e6984574d6ab2d3545292344eaa4b
tomasdem <tomasdembelli@gmail.com>
2019-08-27 23:41:36 +0100

    Update: Comments per PEP 8.

--- ascii_flow_chart.py
+++ ascii_flow_chart.py
@@ -5,10 +5,17 @@ class Item():
     """Provide shared methods and attributes for creating flowchart 
     items.
     
-    Define  allowed maximum item size."""
+    Define boundaries which will override user input:
+    max_X -- the maximum width of an item (default 150 chars)
+    max_Y -- the maximum height of an item (default 20 chars)
+    max_padding_X -- the maximum padding on X axis (default 20 chars)
+    max_padding_Y -- the maximum padding on Y axis (default 4 chars)
+    """
 
     max_X = 150
     max_y = 20
+    max_padding_X = 20
+    max_padding_Y = 4
     
     def __init__(self, x=10, y=5, padding_X=0, padding_Y=0, character_X=' ', 
             character_Y=' ', character_Fill=' ', character_Joint=' ', 
@@ -16,29 +23,43 @@ class Item():
         """Initiate an item.
         
         Keyword arquments:
-        x -- the length (default 10 or wrap the text)
-        y -- the height (default 5 wrap the text)
+        x -- the length (default 10 or long enough to wrap wrap the text)
+        y -- the height (default 5 or long enough to wrap the text)
         padding_X -- default 0
         padding_Y -- default 0
         character_X -- character for the horizantal sides (default space)
         character_Y -- character for the vertical sides (default space)
-        character_Fill -- character filling inside item (default space)
+        character_Fill -- character filling the item (default space)
         character_Joint -- character used at corners (default space)
         text -- the text presented in the item
         """
 
-        self.padding_X = min(abs(int(padding_X)), 20)
-        self.padding_Y = min(abs(int(padding_Y)), 4)
-        self.text = text.replace('\t', '').split('\n')
-        self.min_X = max(list(map(len, self.text))) + 2 + 2*self.padding_X    #2 for borders
+        # Remove tabs from the text and split it into multiple lines.
+        self.text = text.replace('\t', '').split('\n')    
+
+        # Enforce the maximum boundaries for padding.
+        self.padding_X = min(abs(int(padding_X)), Item.max_padding_X)
+        self.padding_Y = min(abs(int(padding_Y)), Item.max_padding_Y)
+
+        # Define the minimum boundaries to ensure that
+        # the size is big enough to wrap the text.
+        self.borders = 2    # The number of berders on each axises.
+        self.longest_text = max(list(map(len, self.text)))
+        self.min_X = self.longest_text + self.borders + 2*self.padding_X  
+        self.min_Y = len(self.text) + self.borders + 2*self.padding_Y
+
+        # Enforce the maximum and minimum boundaries.
         self.x = min(max(abs(int(x)), self.min_X), Item.max_X)
-        self.min_Y = 2*self.padding_Y + 2 + len(self.text)
-        self.y = min(max(abs(int(y)), self.min_Y), Item.max_y)    
+        self.y = min(max(abs(int(y)), self.min_Y), Item.max_y)  
+
+        # Ensure that only one string character is accepted.
         self.character_X = str(character_X)[0]
         self.character_Y = str(character_Y)[0]
         self.character_Fill = str(character_Fill)[0]
         self.character_Joint = str(character_Joint)[0]
-        self.item_Matrix = []
+        
+        # The matrix will be created at specific object initiation.
+        self.item_Matrix = []    
     
     def draw(self):
         """Join the elements of matrix and return a single string."""
@@ -54,8 +75,8 @@ class ItemRectangle(Item):
         """Initiate a rectangular item.
         
         Keyword arquments:
-        x -- the length (default 10 or wrap the text)
-        y -- the height (default 5 or wrap the text)
+        x -- the length (default 10 or long enough to wrap the text)
+        y -- the height (default 5 or long enough to wrap the text)
         padding_X -- default 3
         padding_Y -- default 1
         character_X -- character for the horizantal sides (default -)
@@ -67,17 +88,27 @@ class ItemRectangle(Item):
 
         super().__init__(x, y, padding_X, padding_Y, character_X, 
             character_Y, character_Fill, character_Joint, text)
+
+        # Create the matrix with X, Y and filling characters.
         self.item_Matrix = [[self.character_Y if i in [0, self.x-1] else 
             self.character_Fill if j not in [0, self.y-1] else 
             self.character_X for i in range(self.x)] for j in range(self.y)]
-        for (q, w) in [(0,0), (0, -1), (-1, 0), (-1, -1)]: 
+
+        # Insert the joint characters to the corners.
+        for (q, w) in [(0, 0), (0, -1), (-1, 0), (-1, -1)]: 
             self.item_Matrix[q][w] = self.character_Joint
-        # adding text in to the item
-        if self.text != ['']:
-            for idx, txt in enumerate(self.text):
-                txt = txt[:self.x - 2 - 2*self.padding_X]    #slice the text not to exceed max_X
-                gap_Fill = self.x - self.padding_X - 2 - len(txt)    #2 is for the borders
-                self.item_Matrix[self.padding_Y + 1 + idx] = (
-                    [self.character_Y] + self.padding_X*[character_Fill] 
-                    + list(txt) + gap_Fill*[character_Fill] 
-                    + [self.character_Y])
+
+        # Ad text into the matrix.
+        for idx, txt in enumerate(self.text):
+
+            # Ensure the text is not longer than the item can hold.
+            txt = txt[: self.x - self.borders - 2*self.padding_X]   
+
+            # Calculate the gab between the end of text and the border.
+            gap_Fill = self.x - self.padding_X - self.borders - len(txt)
+
+            # Insert one line of text each time.
+            self.item_Matrix[self.padding_Y + 1 + idx] = (    
+                [self.character_Y] + self.padding_X*[character_Fill] 
+                + list(txt) + gap_Fill*[character_Fill] 
+                + [self.character_Y])
