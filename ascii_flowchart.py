a3597d15ba79365eb9bba20ecadd4de7e9d93701
tomasdem <tomasdembelli@gmail.com>
2019-08-28 07:25:31 +0100

    Delete: dublicate file.

--- ascii_flowchart.py
+++ ascii_flowchart.py
@@ -8,29 +8,29 @@ class Item():
     Define boundaries which will override user input:
     max_X -- the maximum width of an item (default 150 chars)
     max_Y -- the maximum height of an item (default 20 chars)
-    max_padding_X -- the maximum padding on X-axis (default 20 chars)
-    max_padding_Y -- the maximum padding on Y-axis (default 4 chars)
+    max_padding_x -- the maximum padding on X-axis (default 20 chars)
+    max_padding_y -- the maximum padding on Y-axis (default 4 chars)
     """
 
     max_X = 150
     max_y = 20
-    max_padding_X = 20
-    max_padding_Y = 4
+    max_padding_x = 20
+    max_padding_y = 4
     
-    def __init__(self, x=10, y=5, padding_X=0, padding_Y=0, character_X=' ', 
-            character_Y=' ', character_Fill=' ', character_Joint=' ', 
+    def __init__(self, x=10, y=5, padding_x=0, padding_y=0, character_x=' ', 
+            character_y=' ', character_fill=' ', character_joint=' ', 
             text=' '):
         """Initiate an item.
         
         Keyword arquments:
         x -- the length (default 10 or long enough to wrap wrap the text)
         y -- the height (default 5 or long enough to wrap the text)
-        padding_X -- default 0
-        padding_Y -- default 0
-        character_X -- character for the horizantal sides (default space)
-        character_Y -- character for the vertical sides (default space)
-        character_Fill -- character filling the item (default space)
-        character_Joint -- character used at corners (default space)
+        padding_x -- default 0
+        padding_y -- default 0
+        character_x -- character for the horizantal sides (default space)
+        character_y -- character for the vertical sides (default space)
+        character_fill -- character filling the item (default space)
+        character_joint -- character used at corners (default space)
         text -- the text presented in the item
         """
 
@@ -38,77 +38,77 @@ class Item():
         self.text = text.replace('\t', '').split('\n')    
 
         # Enforce the maximum boundaries for padding.
-        self.padding_X = min(abs(int(padding_X)), Item.max_padding_X)
-        self.padding_Y = min(abs(int(padding_Y)), Item.max_padding_Y)
+        self.padding_x = min(abs(int(padding_x)), Item.max_padding_x)
+        self.padding_y = min(abs(int(padding_y)), Item.max_padding_y)
 
         # Define the minimum boundaries to ensure that
         # the size is big enough to wrap the text.
         self.borders = 2    # The number of berders on each axises.
         self.longest_text = max(list(map(len, self.text)))
-        self.min_X = self.longest_text + self.borders + 2*self.padding_X  
-        self.min_Y = len(self.text) + self.borders + 2*self.padding_Y
+        self.min_x = self.longest_text + self.borders + 2*self.padding_x  
+        self.min_y = len(self.text) + self.borders + 2*self.padding_y
 
         # Enforce the maximum and minimum boundaries.
-        self.x = min(max(abs(int(x)), self.min_X), Item.max_X)
-        self.y = min(max(abs(int(y)), self.min_Y), Item.max_y)  
+        self.x = min(max(abs(int(x)), self.min_x), Item.max_X)
+        self.y = min(max(abs(int(y)), self.min_y), Item.max_y)  
 
         # Ensure that only one string character is accepted.
-        self.character_X = str(character_X)[0]
-        self.character_Y = str(character_Y)[0]
-        self.character_Fill = str(character_Fill)[0]
-        self.character_Joint = str(character_Joint)[0]
+        self.character_x = str(character_x)[0]
+        self.character_y = str(character_y)[0]
+        self.character_fill = str(character_fill)[0]
+        self.character_joint = str(character_joint)[0]
         
         # The matrix will be created at specific object initiation.
-        self.item_Matrix = []    
+        self.item_matrix = []    
     
     def draw(self):
         """Join the elements of matrix and return a single string."""
-        return '\n'.join(map(''.join, self.item_Matrix))
+        return '\n'.join(map(''.join, self.item_matrix))
 
 
 class ItemRectangle(Item):
     """Create a rectangle object with optional text in it."""
     
-    def __init__(self, x=10, y=5, padding_X=3, padding_Y=1, character_X='-', 
-            character_Y='|', character_Fill=' ', character_Joint='+', 
+    def __init__(self, x=10, y=5, padding_x=3, padding_y=1, character_x='-', 
+            character_y='|', character_fill=' ', character_joint='+', 
             text=' '):
         """Initiate a rectangular item.
         
         Keyword arquments:
         x -- the length (default 10 or long enough to wrap the text)
         y -- the height (default 5 or long enough to wrap the text)
-        padding_X -- default 3
-        padding_Y -- default 1
-        character_X -- character for the horizantal sides (default -)
-        character_Y -- character for the vertical sides (default |)
-        character_Fill -- character filling inside item (default space)
-        character_Joint -- character used at corners (default +)
+        padding_x -- default 3
+        padding_y -- default 1
+        character_x -- character for the horizantal sides (default -)
+        character_y -- character for the vertical sides (default |)
+        character_fill -- character filling inside item (default space)
+        character_joint -- character used at corners (default +)
         text -- the text presented in the item
         """
 
-        super().__init__(x, y, padding_X, padding_Y, character_X, 
-            character_Y, character_Fill, character_Joint, text)
+        super().__init__(x, y, padding_x, padding_y, character_x, 
+            character_y, character_fill, character_joint, text)
 
         # Create the matrix with X, Y and filling characters.
-        self.item_Matrix = [[self.character_Y if i in [0, self.x-1] else 
-            self.character_Fill if j not in [0, self.y-1] else 
-            self.character_X for i in range(self.x)] for j in range(self.y)]
+        self.item_matrix = [[self.character_y if i in [0, self.x-1] else 
+            self.character_fill if j not in [0, self.y-1] else 
+            self.character_x for i in range(self.x)] for j in range(self.y)]
 
         # Insert the joint characters to the corners.
         for (q, w) in [(0, 0), (0, -1), (-1, 0), (-1, -1)]: 
-            self.item_Matrix[q][w] = self.character_Joint
+            self.item_matrix[q][w] = self.character_joint
 
         # Ad text into the matrix.
         for idx, txt in enumerate(self.text):
 
             # Ensure the text is not longer than the item can hold.
-            txt = txt[: self.x - self.borders - 2*self.padding_X]   
+            txt = txt[: self.x - self.borders - 2*self.padding_x]   
 
             # Calculate the gab between the end of text and the border.
-            gap_Fill = self.x - self.padding_X - self.borders - len(txt)
+            gap_Fill = self.x - self.padding_x - self.borders - len(txt)
 
             # Insert one line of text each time.
-            self.item_Matrix[self.padding_Y + 1 + idx] = (    
-                [self.character_Y] + self.padding_X*[character_Fill] 
-                + list(txt) + gap_Fill*[character_Fill] 
-                + [self.character_Y])
+            self.item_matrix[self.padding_y + 1 + idx] = (    
+                [self.character_y] + self.padding_x*[character_fill] 
+                + list(txt) + gap_Fill*[character_fill] 
+                + [self.character_y])
